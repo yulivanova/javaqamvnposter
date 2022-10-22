f12f0110c1068b5e518bfe3d46f035a75a6a89cf
Юлия <jullka5@mail.ru>
2022-10-22 15:57:00 +0300

    Initial commit

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -1,32 +1,48 @@
 package ru.netology.manager;
 
 import ru.netology.domain.FilmItem;
-import ru.netology.repository.FilmRepository;
 
 public class FilmManager {
-    private final FilmRepository repo;
+    private FilmItem[] items = new FilmItem[0];
+    private int limit;
 
-    public FilmManager(FilmRepository repo) {
-        this.repo = repo;
+    public FilmManager(int limit) {
+        this.limit = limit;
+    }
+
+    public FilmManager() {
+        this.limit = 10;
     }
 
     public void add(FilmItem item) {
-        repo.save(item);
+        int length = items.length + 1;
+        FilmItem[] tmp = new FilmItem[length];
+
+        System.arraycopy(items, 0, tmp, 0, items.length);
+
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
     }
 
-    public FilmItem[] getAll() {
-        FilmItem[] items = repo.findAll();
-        FilmItem[] result = new FilmItem[items.length];
+    public FilmItem[] findAll() {
+
+        return items;
+    }
+
+    public FilmItem[] findLast() {
+        FilmItem[] items = findAll();
+        FilmItem[] result;
+        if (limit < items.length) {
+            result = new FilmItem[limit];
+        } else {
+            result = new FilmItem[items.length];
+        }
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
     }
-
-    public void removeById(int id) {
-        repo.removeById(id);
-    }
 }
-
 
--- src/test/java/ru/netology/manager/FilmManagerTest.java
+++ src/test/java/ru/netology/manager/FilmManagerTest.java
@@ -1,18 +1,10 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import ru.netology.repository.FilmRepository;
 import ru.netology.domain.FilmItem;
 
-import static org.mockito.Mockito.*;
-
 class FilmManagerTest {
-
-    FilmRepository repo = Mockito.mock(FilmRepository.class);
-    FilmManager manager = new FilmManager(repo);
 
     FilmItem film1 = new FilmItem(1, 11, "Бладшот");
     FilmItem film2 = new FilmItem(2, 22, "Вперед");
@@ -24,10 +16,12 @@ class FilmManagerTest {
     FilmItem film8 = new FilmItem(8, 88, "Бой с тенью");
     FilmItem film9 = new FilmItem(9, 99, "Море");
     FilmItem film10 = new FilmItem(10, 100, "Пианист");
-    FilmItem film11 = new FilmItem(11, 111, "<Большой Куш>");
 
-    @BeforeEach
-    public void setUp() {
+
+    @Test
+    void shouldFindAll() {
+        FilmManager manager = new FilmManager();
+
         manager.add(film1);
         manager.add(film2);
         manager.add(film3);
@@ -38,25 +32,97 @@ class FilmManagerTest {
         manager.add(film8);
         manager.add(film9);
         manager.add(film10);
-        manager.add(film11);
+
+        FilmItem[] actual = manager.findAll();
+        FilmItem[] expected = {film1, film2, film3, film4, film5, film6, film7, film8, film9, film10};
+
+        Assertions.assertArrayEquals(actual, expected);
     }
 
     @Test
-    public void shouldLastRemove() {
+    void shouldGetAll() {
+    }
 
-        FilmItem[] items = new FilmItem[]{film1, film2, film4, film5, film6, film7, film8, film9, film10, film11};
+    @Test
+    void shouldFindLast() {
+        FilmManager manager = new FilmManager();
 
-        manager.removeById(film3.getId());
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+        manager.add(film6);
+        manager.add(film7);
+        manager.add(film8);
+        manager.add(film9);
+        manager.add(film10);
 
-        doReturn(items).when(repo).findAll();
-        doNothing().when(repo).removeById(film3.getId());
 
-        FilmItem[] expected = new FilmItem[]{film11, film10, film9, film8, film7, film6, film5, film4, film2, film1};
-        FilmItem[] actual = manager.getAll();
-        Assertions.assertArrayEquals(expected, actual);
+        FilmItem[] actual = manager.findLast();
+        FilmItem[] expected = {film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
 
-        verify(repo).removeById(film3.getId());
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldFindLast1() {
+        FilmManager manager = new FilmManager();
+
+        manager.add(film5);
+
+        FilmItem[] actual = manager.findLast();
+        FilmItem[] expected = {film5};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldFindLast3() {
+        FilmManager manager = new FilmManager();
+
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+
+
+        FilmItem[] actual = manager.findLast();
+        FilmItem[] expected = {film5, film4, film3, film2, film1};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldFindLast4() {
+        FilmManager manager = new FilmManager();
+
+        FilmItem[] actual = manager.findLast();
+        FilmItem[] expected = {};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldFindLast5() {
+        FilmManager manager = new FilmManager();
+
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+        manager.add(film6);
+        manager.add(film7);
+        manager.add(film8);
+        manager.add(film9);
+        manager.add(film10);
+
+        FilmItem[] actual = manager.findLast();
+        FilmItem[] expected = {film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
+
+        Assertions.assertArrayEquals(actual, expected);
     }
 }
-
 
